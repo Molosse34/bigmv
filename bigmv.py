720dde7cd66202e99248294bf809df9383103f2d
Molosse34 <112559872+Molosse34@users.noreply.github.com>
2022-09-30 14:53:51 +0200

    Add files via upload

---
+++ bigmv.py
@@ -0,0 +1,11 @@
+import os.path as op
+import os
+
+#files faut qu'il soit sous la forme /my/file
+#path faut qu'il soit sous la forme /my/path
+def bigmv(file,path):
+	a=op.exists(path)
+	if a==True:
+		os.system(f"mv {file} {path} ")
+	if a==False:
+		os.system(f"mkdir {path} && mv {file} {path} ")
